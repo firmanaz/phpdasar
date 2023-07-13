8c4741215dd3975813db7ec76892d8d53c4e05ec
Firman Abdul Zaelani <firmanaz55@gmail.com>
2023-07-13 08:19:10 +0700

    latihan array

---
+++ pertemuan5/latihan1.php
@@ -0,0 +1,17 @@
+<?php 
+    // array
+    // variabel yang dapat memiliki banyak nilai
+    // elemen pada array boleh memiliki tipe data yang berbeda
+
+    // membuat array 
+    // cara lama 
+    // key-nya adalah index, yang dimulai dari 0
+    $hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
+    // cara baru
+    $bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]; 
+    $arr1 = [123, "tulisan", false];
+
+    // menampilkan array
+    // var_dump / print_r
+    var_dump($hari);
+?>
